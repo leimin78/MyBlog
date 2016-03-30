899b1c646f408f81964e3ffd91fe47580a124d2d
leimin78 <18898761417@139.com>
2016-03-30 22:10:51 +0800

    3月30日更新发布到亚马逊主机

--- MyBlog.py
+++ MyBlog.py
@@ -1,20 +1,23 @@
 ﻿# encoding:utf-8
 
-import time
 from flask import Flask,render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 #装饰器，根目录
 
 @app.route('/')
 def index():
-	return render_template('base.html')
+	return render_template('index.html',current_time=datetime.utcnow())
 	
 #装饰器，用户目录
 @app.route('/user/<name>')
