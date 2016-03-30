100ecea07fe2840ddb315f28f747bc335b677eb2
leimin78 <18898761417@139.com>
2016-03-30 12:55:41 +0800

    修改支持图标显示

--- MyBlog.py
+++ MyBlog.py
@@ -1,17 +1,41 @@
-# encoding:utf-8
+﻿# encoding:utf-8
 
 import time
 from flask import Flask,render_template
-
+from flask.ext.bootstrap import Bootstrap
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+#装饰器，根目录
+
+@app.route('/')
+def index():
+	return render_template('base.html')
+	
+#装饰器，用户目录
 @app.route('/user/<name>')
 def user(name):
-	mydict = {'key':'flask'}
-	mylist = [1,2,3,'myintvar',5]
-	comments = ['Python2.7','Python3.0']
-	return render_template('user.html', name=name, mydict=mydict , mylist=mylist,comments=comments )
+	return render_template('user.html',name=name)
 	
+#装饰器，静态文件
+@app.route('/static/')
+def sendstatic():
+	return render_template('base.html')
+	
+#装饰器，404 及500
+@app.errorhandler(404)
+def page_notfound(e):
+	return render_template('404.html')
+	
+@app.errorhandler(500)
+def Server_Error(e):
+	return render_template('500.html')
+	
+
+#主函数
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ readme.txt
@@ -0,0 +1,6 @@
+#这是我的第一个自建的Blog，基于 Python + Flask
+#记录Python学习的过程
+
+####将代码push到github仓库 #####
+git remote add origin https://github.com/leimin78/MyBlog.git
+git push -u origin master
